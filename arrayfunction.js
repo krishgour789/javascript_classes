b4455dcca1b33a7d9a1e84c0c0bcfda9f0a23083
krishgour789 <krishgour789@gmail.com>
2025-11-03 15:20:06 -0800

    added todzys

---
+++ arrayfunction.js
@@ -0,0 +1,14 @@
+ar = ['JS','REACT','DJANGO','PYTHON']
+console.log(ar)
+
+ar.push("JAVA","hTML")
+console.log(ar)
+
+ar.pop()
+console.log(ar)
+
+ar.unshift("C","ASSEMBLE") // it add in first
+console.log(ar)
+
+ar.shift() // its remove the first element
+console.log(ar)
---
+++ forof.js
@@ -0,0 +1,22 @@
+// with the help of forof we dont need indexing . we can fetch elements by forof
+// let ar = ['krish',34,'indore',67]
+// for(let vl of ar){
+//     console.log(vl)
+// }
+
+// q1.make a array 25-1 ,even number and sum its access
+
+// let ar = [25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+
+// for (let vl of ar){
+//     if(vl%2==0){
+//         console.log(vl*vl)
+//     }
+// }
+
+
+// with the help of forin we can fetch indexing numbers from this.
+let ar = ['krish',34,'indore',67]
+for(let vl in ar){
+    console.log(vl)
+}
