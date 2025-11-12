fc524a89606aa5c6e6d136ca122343cf5251f344
krishgour789 <krishgour789@gmail.com>
2025-11-12 17:29:40 +0530

    Mathsmodule

--- api.js
+++ api.js
@@ -46,3 +46,18 @@
 // }
 // access()
 // template literals = ''
+
+async function access(){
+    let data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let res = await data.json()
+    let result = res.map((e)=>`
+    <tr>
+    <td> ${e.userId} </td>
+    <td> ${e.id} </td>
+    <td> ${e.title} </td>
+</tr>`
+).join("")
+document.querySelector('#showdata').innerHTML=result
+}
+access()
+
---
+++ mathmodule.js
@@ -0,0 +1,17 @@
+console.log(Math.pow(3,2))
+console.log(Math.sqrt(16))
+console.log(Math.PI)
+console.log(Math.min(3,12,3,54,5,2))
+console.log(Math.max(1,4,5,6,34,3,63,2))
+console.log(Math.trunc(10.34))
+console.log(Math.trunc(Math.random()*10+1))
+
+let today = new Date()
+console.log(today)
+console.log(today.getFullYear())
+let console.log(today.getMonth()+1)
+console.log(today.getDate())
+console.log(today.toLocaleTimeString())
+console.log(today.toLocaleDateString())
+
+/// homework ... write a   
